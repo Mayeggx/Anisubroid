39347b5cb7ac45b7058ce66a7cb1b3ba51c19df0
Mayegg <1498638223@qq.com>
2026-05-11 04:55:49 +0800

    Release v1.0.8

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.mayegg.anisub"
         minSdk = 26
         targetSdk = 34
-        versionCode = 9
-        versionName = "1.0.7"
+        versionCode = 10
+        versionName = "1.0.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
